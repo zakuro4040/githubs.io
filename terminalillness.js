227efdc6db4536bbeda72b0f730774b848065cdc
zakuro4040 <96808221+zakuro4040@users.noreply.github.com>
2022-01-31 18:46:35 +0900

    Create terminalillness.js

---
+++ terminalillness.js
@@ -0,0 +1,27 @@
+$(function() {
+    $(".mainvisual-logo").css("display","none");
+    $(".mainvisual-logo").fadeIn(1500);
+    
+    /*header-nav*/
+    $("#about").click(function(){
+        var about=$(this).attr(href);
+        var position=$(id).offset().about;
+    })
+
+    $("#event").click(function(){
+        var about=$(this).attr(href);
+        var position=$(id).offset().about;
+    })
+
+    $("#member").click(function(){
+        var about=$(this).attr(href);
+        var position=$(id).offset().about;
+    })
+
+    $("#coback").click(function(){
+        var about=$(this).attr(href);
+        var position=$(id).offset().about;
+    })
+
+  });
+  
